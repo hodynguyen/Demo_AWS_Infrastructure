02e91ff364025d495646e8a70ef6418afd72f744
hodynguyen <nguyenthanhdat23012003@gmail.com>
2025-11-24 02:49:26 +0700

    Create Private Route Table

--- infra/vpc/route_tables.tf
+++ infra/vpc/route_tables.tf
@@ -24,3 +24,30 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# -----------------------------------------
+# PRIVATE ROUTE TABLE
+# -----------------------------------------
+
+# 1. Create Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "acme-private-rt"
+  }
+}
+
+# 2. Route private outbound → NAT Gateway
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+# 3. Associate private subnets to private route table
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private[*].id)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
