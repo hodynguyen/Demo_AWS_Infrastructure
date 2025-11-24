1e75d5e5602026beea231ddd6562035c16621cb2
hodynguyen <nguyenthanhdat23012003@gmail.com>
2025-11-24 09:58:27 +0700

    Add API SG

--- infra/vpc/security_groups.tf
+++ infra/vpc/security_groups.tf
@@ -61,3 +61,48 @@ resource "aws_security_group" "ui" {
 
   tags = { Name = "acme-ui-sg" }
 }
+
+# -----------------------------------------
+# API SECURITY GROUP
+# -----------------------------------------
+resource "aws_security_group" "api" {
+  name        = "acme-api-sg"
+  description = "Allow traffic from ALB and outbound to DB/Metrics"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  ingress {
+    description     = "HTTPS from ALB"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  # Outbound: to DB
+  egress {
+    description     = "API calls PostgreSQL"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.db.id]
+  }
+
+  # Outbound: to Metrics
+  egress {
+    description     = "Send metrics"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.metrics.id]
+  }
+
+  tags = { Name = "acme-api-sg" }
+}
